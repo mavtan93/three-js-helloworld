01222e11e77b21bf5ec9f0edce60d1a452551380
Tan <mavric.tan93@gmail.com>
2025-05-24 23:09:28 +0800

    fix deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [],
+    base: '/three-js-helloworld/' // for GitHub Pages deployment
+})
