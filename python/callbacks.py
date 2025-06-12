568b733a28457cefac818e8e8b39d008e81983d8
Tan <mavric.tan93@gmail.com>
2025-06-12 08:22:45 +0800

    add more comments for callback

--- python/callbacks.py
+++ python/callbacks.py
@@ -9,10 +9,17 @@ def my_callback(result):
 
 # Function that takes a callback as an argument
 def do_something(data, callback):
+    print(f"Doing something with data: {data}")
     # Do something with data
     result = data * 2
+    
     # Call the callback with the result
+    print("Calling the callback...")
     callback(result)
+    print("Callback has been called.")
+
+    print("Finished doing something.")
+
 
 # Example usage
 do_something(5, my_callback)
